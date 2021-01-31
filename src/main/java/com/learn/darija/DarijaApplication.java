085d037f354ef0af556f7ea29975d562ce779459
zsouiki <zsouiki>
2021-01-31 23:29:12 +0100

    INIT PROJECT with spring initializer

---
+++ src/main/java/com/learn/darija/DarijaApplication.java
@@ -0,0 +1,13 @@
+package com.learn.darija;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarijaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DarijaApplication.class, args);
+    }
+
+}
